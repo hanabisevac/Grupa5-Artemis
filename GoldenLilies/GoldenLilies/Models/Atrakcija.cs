8b3999575f75175e3e77a50be449dc9667d9514c
Anida Nezovic <anezovic1@etf.unsa.ba>
2022-06-11 14:49:02 +0200

    Ispravljeni nazivi za atribute klase Atrakcija

--- GoldenLilies/GoldenLilies/Models/Atrakcija.cs
+++ GoldenLilies/GoldenLilies/Models/Atrakcija.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,16 +8,26 @@ namespace GoldenLilies.Models
     public class Atrakcija
     {
         [Key]
-        public int ID { get; set; }    
+        public int ID { get; set; }
+
+        [DisplayName("Naziv atrakcije")]
         public string naziv { get; set; }
         [ForeignKey("Lokacija")]
         public int lokacijaID { get; set; }
 
         [ForeignKey("VrstaAtrakcije")]
         public int vrstaAtrakcijeID { get; set; }
+        
+        [DisplayName("Osnovne informacije")]
         public string informacije { get; set; }
+
+        [DisplayName("Radno vrijeme")]
         public DateTime radnoVrijeme { get; set; }
+
+        [DisplayName("Lokacija")]
         public Lokacija lokacija { get; set; }
+
+        [DisplayName("Vrsta atrakcije")]
         public VrstaAtrakcije vrstaAtrakcije { get; set; }
 
         public Atrakcija() { }
